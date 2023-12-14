9961d95de7b543f5951be422f9b878d4ebf40f10
mialinm <98932148+mialinm@users.noreply.github.com>
2023-12-13 23:31:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+const db = require('./db')
 const express = require("express")
 
 const app = express()
 
 const port = 3000
 
+app.get('/api/links', db.getLinks)
+
 app.get('/', (req, res)=> {
     res.send("Hello World! We're live on our first server!")
 })
 
+app.post('/api/links', db.createLink);
+
+app.put('/api/links/:id', db.updateLink);
+
+app.delete('/api/links/:id', db.deleteLink);
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
+
